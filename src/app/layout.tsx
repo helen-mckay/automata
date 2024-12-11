1128f2ac4836a030eb14038eb80fd3370d49acc4
Helen McKay <helen.mckay@comcast.net>
2024-12-11 16:34:01 -0600

    spacing, semicolon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { Provider } from "@/components/ui/provider"
 
 export default function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
+  
+  const { children } = props;
+
   return (
     <html suppressHydrationWarning>
       <body>

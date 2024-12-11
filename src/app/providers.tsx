c8220e113764ad16a7e5a0789c8f52a206dc1265
Helen McKay <helen.mckay@comcast.net>
2024-12-11 16:24:34 -0600

    set up chakra ui

---
+++ src/app/providers.tsx
@@ -0,0 +1,9 @@
+// app/providers.tsx
+'use client'
+
+import { ChakraProvider } from '@chakra-ui/react'
+import { defaultSystem } from '@chakra-ui/react'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
+}

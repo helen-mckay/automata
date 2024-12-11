5025c8a6418867c0fb91513166cf41e71154ac86
Helen McKay <helen.mckay@comcast.net>
2024-12-11 16:39:24 -0600

    placeholder text for intro section

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,7 +3,9 @@ import Section from "../Section";
 const Intro = () => {
     return(
         <Section>
-             <div>Intro</div>
+            <h1>Intro</h1>
+            <p>Minim dolor fugiat eu ipsum sint eiusmod et in voluptate ut mollit reprehenderit deserunt id. Ex anim proident amet est tempor adipisicing occaecat consequat laborum nisi aute ea. Voluptate ea duis reprehenderit sunt nisi officia dolore sit incididunt consequat quis sint. Duis deserunt ipsum excepteur aliquip ut id.</p>
+            <p>Commodo cupidatat sit incididunt Lorem duis amet sunt consequat ex minim proident magna. Tempor non anim tempor mollit voluptate veniam dolore duis magna eiusmod et id. Ullamco ipsum minim tempor exercitation duis minim deserunt magna ad ad reprehenderit. Et culpa est duis velit. Lorem do commodo veniam nostrud adipisicing anim officia. Officia in aliqua ex magna laboris in sit sit voluptate consectetur laborum consequat culpa sunt.</p>
         </Section>
     );
 }
